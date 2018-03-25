e2c709cbc39fea839de7a1099c6d887a1d73bbe7
Leonard Dalmacio <leodalmacio19@gmail.com>
2018-03-25 15:43:52 +0800

    Added database yelp_camp with collection of campgrounds. Made the index, new, create, and show routes of RESTful Routes

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
-var campgrounds = [
-	{name: "Salmon Creek", image: "https://www.dl-online.com/sites/default/files/styles/16x9_620/public/field/image/Aug.Sept_.SummerScene.Camping%20%281%29.jpg?itok=4bWG5K3R"},
-	{name: "Granite Hill", image: "http://www.hiddenvalleydeadwood.com/sites/hiddenvalley/files/styles/navigation_tile_smart_phone/public/photos/amenity/camping/0150-hidden-valley-campground-2014-08-12.jpg?itok=z-Wrp7cd&timestamp=1514484420"},
-	{name: "Montain Goat's Rest", image: "https://www.dl-online.com/sites/default/files/styles/16x9_620/public/field/image/Aug.Sept_.SummerScene.Camping%20%281%29.jpg?itok=4bWG5K3R"},
-	{name: "Zion's Gorge", image: "https://www.dl-online.com/sites/default/files/styles/16x9_620/public/field/image/Aug.Sept_.SummerScene.Camping%20%281%29.jpg?itok=4bWG5K3R"},
-		{name: "Salmon Creek", image: "https://www.dl-online.com/sites/default/files/styles/16x9_620/public/field/image/Aug.Sept_.SummerScene.Camping%20%281%29.jpg?itok=4bWG5K3R"},
-	{name: "Granite Hill", image: "https://www.dl-online.com/sites/default/files/styles/16x9_620/public/field/image/Aug.Sept_.SummerScene.Camping%20%281%29.jpg?itok=4bWG5K3R"},
-	{name: "Montain Goat's Rest", image: "https://www.dl-online.com/sites/default/files/styles/16x9_620/public/field/image/Aug.Sept_.SummerScene.Camping%20%281%29.jpg?itok=4bWG5K3R"},
-	{name: "Zion's Gorge", image: "https://www.dl-online.com/sites/default/files/styles/16x9_620/public/field/image/Aug.Sept_.SummerScene.Camping%20%281%29.jpg?itok=4bWG5K3R"},
-];
+var express 				= require("express"),
+		app 						= express(),
+		bodyParser 			= require("body-parser"),
+		mongoose 				= require("mongoose");
+
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+// 	{
+// 		name: "Granite Hill", 
+// 		image: "http://www.hiddenvalleydeadwood.com/sites/hiddenvalley/files/styles/navigation_tile_smart_phone/public/photos/amenity/camping/0150-hidden-valley-campground-2014-08-12.jpg?itok=z-Wrp7cd&timestamp=1514484420",
+// 		description: "This is a huge granite hill, no bathrooms. No water. Beautiful Granite!",
+// 	},
+// 	function (err, campground){
+// 		if(err) {
+// 			console.log(err);
+// 		}
+// 		else {
+// 			console.log("Newly created campground!");
+// 			console.log(campground);
+// 		}
+// 	}
+// );
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -22,23 +41,48 @@ app.get("/", function(req,res){
 	res.render("landing");
 });
 
+// INDEX ROUTE - place where all campgrounds are shown, the index or root of all campgrounds
 app.get("/campgrounds", function(req, res) {
-	res.render("campgrounds", {campgrounds: campgrounds});
+	Campground.find({}, function(err, campgrounds){
+		if (err) {
+			console.log("Something went wrong in finding campground");
+		} else {
+			res.render("index", {campgrounds: campgrounds});
+		}
+	});
 });
 
+// CREATE ROUTE - saves that data from NEW ROUTE to the DB
 app.post("/campgrounds", function(req, res) {
 	var name = req.body.name;
 	var image = req.body.image;
-	var newCampground = {name: name, image: image};
+	var desc = req.body.description;
+	var newCampground = {name: name, image: image, description: desc};
 
-	campgrounds.push(newCampground);
-	res.redirect("/campgrounds");
+	Campground.create(newCampground, function(err, newlyCreated){
+		if (err) {
+			console.log(err);
+		} else {
+			res.redirect("/campgrounds");
+		}
+	});
 });
 
+// NEW ROUTE - the form where to save the data
 app.get("/campgrounds/new", function(req, res) {
 	res.render("new");
 });
 
+// SHOW  ROUTE - shows info about one campground, ** ORDER OF ROUTE IS IMPORTANT! **
+app.get("/campgrounds/:id", function(req, res) {
+	Campground.findById(req.params.id, function(err, foundCampground){
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("show", {campground: foundCampground});
+		}
+	});
+});
 
 //============= END ROUTES ============///
 
